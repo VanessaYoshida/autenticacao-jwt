ed3de0a7cb1378a51f4c6df6b0c57d9ac4424bd4
Vanessa Yoshida <vanessayoshida92@gmail.com>
2022-06-18 16:02:51 -0300

    subindo estrutura do projeto

---
+++ microsservico-autenticacao-jwt/src/main/java/com/project/microserviceautenticationjwt/microsservicoautenticacaojwt/MicrosservicoAutenticacaoJwtApplication.java
@@ -0,0 +1,13 @@
+package com.project.microserviceautenticationjwt.microsservicoautenticacaojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosservicoAutenticacaoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosservicoAutenticacaoJwtApplication.class, args);
+	}
+
+}
---
+++ microsservico-autenticacao-jwt/src/test/java/com/project/microserviceautenticationjwt/microsservicoautenticacaojwt/MicrosservicoAutenticacaoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microserviceautenticationjwt.microsservicoautenticacaojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosservicoAutenticacaoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
