85b1175830b03970cbb561c3ce85f5cb52fda231
Vanessa Yoshida <vanessayoshida92@gmail.com>
2022-06-20 21:58:01 -0300

    Implementação de detalhe do usuario

---
+++ src/main/java/com/project/microserviceautenticationjwt/microsservicoautenticacaojwt/service/DetalheUsuarioServiceImpl.java
@@ -0,0 +1,32 @@
+package com.project.microserviceautenticationjwt.microsservicoautenticacaojwt.service;
+
+import com.project.microserviceautenticationjwt.microsservicoautenticacaojwt.data.DetalheUsuarioData;
+import com.project.microserviceautenticationjwt.microsservicoautenticacaojwt.model.UsuarioModel;
+import com.project.microserviceautenticationjwt.microsservicoautenticacaojwt.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DetalheUsuarioServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    public DetalheUsuarioServiceImpl(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional< UsuarioModel> usuario = repository.findByLogin(username);
+        if (usuario.isEmpty()){
+            throw new UsernameNotFoundException("Usuário [" + username + "] não encontrado");
+        }
+        return new DetalheUsuarioData(usuario
+        );
+    }
+}
